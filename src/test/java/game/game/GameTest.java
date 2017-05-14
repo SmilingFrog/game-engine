5ebc9a1f502fbf66456dfbf1aae78d952c3d846c
Sergii Savin <nissserge@gmail.com>
2017-05-14 12:35:52 +0200

    clean up

--- src/test/java/game/game/GameTest.java
+++ src/test/java/game/game/GameTest.java
@@ -2,33 +2,37 @@ package game.game;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
+	GameBuilder builder;
+	
+	@Before
+	public void setup(){
+		builder = Game.getGameBuilder();
+	}
+	
 	@Test
 	public void canCreateGameBuilder() {
-		GameBuilder builder = Game.getGameBuilder();
 		assertNotNull(builder);
 	}
 	
 	@Test
 	public void canCreateGameWithBuilder(){
-		GameBuilder builder = Game.getGameBuilder();
 		Game game = builder.build();
 		assertNotNull(game);
 	}
 	
 	@Test
 	public void gameBuilderHasId(){
-		GameBuilder builder = Game.getGameBuilder();
 		assertNotEquals("0", builder.getId());
 		assertNotNull(builder.getId());
 	}
 	
 	@Test
 	public void saveBuilderInRepository(){
-		GameBuilder builder = Game.getGameBuilder();
 		GameBuilderRepository repo = new GameBuilderRepositoryImpl();
 		assertEquals(0, repo.size());
 		repo.save(builder);
